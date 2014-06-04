8fb78d85e313e4a7b433b4148958bf595385e0fb
Joshua Howland <development@jkhowland.com>
2014-06-03 23:41:17 -0600

    Initial Commit

---
+++ Autocorrect/ACAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ACAppDelegate.h
+//  Autocorrect
+//
+//  Created by Joshua Howland on 6/3/14.
+//  Copyright (c) 2014 DevMountain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ACAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
